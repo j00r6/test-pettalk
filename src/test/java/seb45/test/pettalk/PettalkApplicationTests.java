de8325e5dc56977e525a157a53a43151d7e906b3
hoya2222 <fkfk1285@naver.com>
2023-09-15 22:45:02 +0900

    init

---
+++ src/test/java/seb45/test/pettalk/PettalkApplicationTests.java
@@ -0,0 +1,13 @@
+package seb45.test.pettalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
